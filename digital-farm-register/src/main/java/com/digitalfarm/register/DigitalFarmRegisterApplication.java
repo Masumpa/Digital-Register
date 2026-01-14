67c76af31c3362691cc5f7271657351ebb9d81e3
Nkosingiphile Mbhele <143130920+Nkosip@users.noreply.github.com>
2026-01-14 15:09:29 +0200

    Initial commit

---
+++ digital-farm-register/src/main/java/com/digitalfarm/register/DigitalFarmRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.digitalfarm.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalFarmRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalFarmRegisterApplication.class, args);
+	}
+
+}
---
+++ digital-farm-register/src/test/java/com/digitalfarm/register/DigitalFarmRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalfarm.register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalFarmRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
